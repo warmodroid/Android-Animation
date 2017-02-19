9926f6f8e58e8781dff6c196a3b2d3e81ea152f1
Mohit Agrawal <mohit.agrawal886@gmail.com>
2017-02-19 12:18:14 +0530

    floatable activity on homescreen in android

--- VogellaAnimation/app/src/main/java/com/example/warmodroid/vogellaanimation/FloatingFaceBubbleService.java
+++ VogellaAnimation/app/src/main/java/com/example/warmodroid/vogellaanimation/FloatingFaceBubbleService.java
@@ -2,53 +2,105 @@ package com.example.warmodroid.vogellaanimation;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class FloatingFaceBubbleService extends Service {
     private WindowManager windowManager;
-    private ImageView floatingFaceBubble;
+    private LinearLayout ll;
+    private Button stop;
+    private WebView webView;
+    private long touchStartTime;
+
+
+    //private ImageView floatingFaceBubble;
     public void onCreate() {
         super.onCreate();
-        floatingFaceBubble = new ImageView(this);
+        //floatingFaceBubble = new ImageView(this);
         //a face floating bubble as imageView
-        floatingFaceBubble.setImageResource(R.mipmap.ic_launcher);
+        //floatingFaceBubble.setImageResource(R.mipmap.ic_launcher);
         windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+        ll = new LinearLayout(this);
+        stop = new Button(this);
+        webView = new WebView(this);
+
+
+        //params for webview
+        ViewGroup.LayoutParams webLayoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        webView.setLayoutParams(webLayoutParams);
+        webView.loadUrl("http://www.google.com");
+
+
+
+
+        //params for button;
+        ViewGroup.LayoutParams btnParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        stop.setText("Close");
+        stop.setLayoutParams(btnParams);
+
+        //params for linear layout
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        ll.setBackgroundColor(Color.argb(66,255,0,0));
+        ll.setLayoutParams(layoutParams);
+
+
+
         //here is all the science of params
         final WindowManager.LayoutParams myParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+                450,450,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
         myParams.gravity = Gravity.TOP | Gravity.LEFT;
         myParams.x=0;
-        myParams.y=100;
+        myParams.y=0;
+
+
+        //ll.addView(webView);
+        //ll.addView(stop);
+        
+        
+        
         // add a floatingfacebubble icon in window
-        windowManager.addView(floatingFaceBubble, myParams);
+        windowManager.addView(webView, myParams);
         try{
             //for moving the picture on touch and slide
-            floatingFaceBubble.setOnTouchListener(new View.OnTouchListener() {
+            webView.setOnTouchListener(new View.OnTouchListener() {
                 WindowManager.LayoutParams paramsT = myParams;
                 private int initialX;
                 private int initialY;
                 private float initialTouchX;
                 private float initialTouchY;
-                private long touchStartTime = 0;
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
                     //remove face bubble on long press
+                    /*
                     if(System.currentTimeMillis()-touchStartTime> ViewConfiguration.getLongPressTimeout() && initialTouchX== event.getX()){
                         windowManager.removeView(floatingFaceBubble);
+
+                        //Toast.makeText(v.getContext(),"clicked",Toast.LENGTH_LONG).show();
                         stopSelf();
                         return false;
                     }
+                    */
+                   // Toast.makeText(v.getContext(),"clicked",Toast.LENGTH_LONG).show();
+
                     switch(event.getAction()){
                         case MotionEvent.ACTION_DOWN:
                             touchStartTime = System.currentTimeMillis();
@@ -63,14 +115,23 @@ public class FloatingFaceBubbleService extends Service {
                             myParams.x = initialX + (int) (event.getRawX() - initialTouchX);
                             myParams.y = initialY + (int) (event.getRawY() - initialTouchY);
                             windowManager.updateViewLayout(v, myParams);
+
                             break;
                     }
                     return false;
                 }
             });
+            stop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    windowManager.removeView(ll);
+                    stopSelf();
+                }
+            });
         } catch (Exception e){
             e.printStackTrace();
         }
+
     }
     @Override
     public IBinder onBind(Intent intent) {
---
+++ VogellaAnimation/app/src/main/java/com/example/warmodroid/vogellaanimation/web.java
@@ -0,0 +1,13 @@
+package com.example.warmodroid.vogellaanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class web extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+}
